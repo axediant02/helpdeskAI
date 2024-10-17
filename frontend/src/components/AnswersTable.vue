b6421595f45c0ca3fcefa2f7794b1d92ef8def37
Ian Caballero <ianfredcaballero@gmail.com>
2024-10-18 00:36:28 +0800

    made the response better

---
+++ frontend/src/components/AnswersTable.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col">
+        <table class="min-w-full divide-y divide-gray-200 shadow-md rounded-lg overflow-hidden">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
+                        Answer
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                <tr v-for="answer in mockAnswers" :key="answer" class="hover:bg-gray-50">
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{ answer }}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+const mockAnswers = [
+    "Answer 1",
+    "Answer 2",
+    "Answer 3",
+    "Answer 4",
+    "Answer 5",
+]
+</script>
---
+++ frontend/src/components/QuestionTable.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col">
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
+                        Question
+                    </th>
+                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium tracking-wider text-gray-500 uppercase">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody class="bg-white divide-y divide-gray-200">
+                <tr v-for="question in mockQuestions" :key="question" class="hover:bg-gray-50">
+                    <td class="px-6 py-4 whitespace-nowrap">
+                        <div class="text-sm font-medium text-gray-900">
+                            {{ question }}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup>
+const mockQuestions = [
+    "What is the library's opening hour?",
+    "Where is the cafeteria located?",
+    "How do I register for classes?",
+    "What are the office hours for the registrar?",
+    "Can I park on campus?"
+]
+</script>
