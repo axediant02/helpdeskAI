1a08ae76b76c661cf05156bdf290706a76fa021a
Ian Caballero <ianfredcaballero@gmail.com>
2024-11-06 12:43:54 +0800

    login ui

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -4,7 +4,6 @@
         <ul>
           <li><router-link to="/">Home</router-link></li>
           <li><router-link to="/about">About</router-link></li>
-          <li><router-link to="/faq">FAQ</router-link></li>
           <li><router-link to="/contact">Contact Us</router-link></li>
           <li><router-link to="/help">Help</router-link></li>
         </ul>
