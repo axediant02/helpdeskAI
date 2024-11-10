fda03d83b7e8e6a63540f0716e79105c72904665
Ian Caballero <ianfredcaballero@gmail.com>
2024-11-10 21:27:27 +0800

    UsersSeeder update

--- backend/database/seeders/UserSeeder.php
+++ backend/database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,11 +17,13 @@ class UserSeeder extends Seeder
         User::factory()->create([
             'name' => 'admin',
             'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
             'role' => 'admin',
         ]);
         User::factory()->create([
             'name' => 'user',
             'email' => 'user@user.com',
+            'password' => Hash::make('password'),
             'role' => 'student',
         ]);
     }
